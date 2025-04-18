c0112df12c8c5f3481cc3c29ec043e9fd250119b
AhM3dAb <ahmedaboud2@gmail.com>
2025-04-18 15:49:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express(); 
 // serves files from the root directory
 app.use(express.static('./'));
-app.listen(3000, function () {    
-  console.log('Listening at http://localhost:3000');  
+app.listen(8080, function () {    
+  console.log('Listening at http://localhost:8080');  
 });
